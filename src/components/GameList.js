4247b472044ffb9948f680a3e189a56a5d1a34df
Marvin Deckmyn <marvindeckmyn@gmail.com>
2023-05-15 16:48:55 +0200

    Show new games

--- src/components/GameList.js
+++ src/components/GameList.js
@@ -181,7 +181,10 @@ const GameList = () => {
                   />
                 </div>
                 <div className="game-info">
-                  <h2 className="game-title">{getTranslatedName(game.translations, game.name)}</h2>
+                  <h2 className="game-title">
+                    {getTranslatedName(game.translations, game.name)}
+                    {game.new && <span className="new-indicator">New!</span>}
+                  </h2>
                   <p className="game-details">
                     {getTranslatedName(game.categoryTranslations, game.category)} • {game.player_count} {t('GameList.minPlayers')}
                   </p>
