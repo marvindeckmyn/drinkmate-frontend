222298b7beb73070c9ed15960c3bac7b08ee4947
Marvin Deckmyn <marvindeckmyn@gmail.com>
2023-06-21 01:01:28 +0200

    Fix SEO and favicone

--- src/components/GameDetails.js
+++ src/components/GameDetails.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import config from '../config';
 import { useTranslation } from 'react-i18next';
 import parse from 'html-react-parser';
+import { Helmet } from 'react-helmet';
 
 const GameDetails = () => {
   const [game, setGame] = useState(null);
@@ -63,47 +64,55 @@ const GameDetails = () => {
   };
 
   return (
-    <div className="main">
-      <div className="game-more-details">
-        <h1>{getTranslatedName(game.translations, game.name)}</h1>
-        <div className="img-wrapper">
-          <img
-            src={`${config.API_BASE_URL}/games/${game.image}`}
-            alt={game.name}
-          />
-        </div>
-        {game.alias && (
-          <p className="alias">
-            {t('GameDetails.alias')}: {getTranslatedAlias(game.aliases, game.alias)}
-          </p>
-        )}
-        <div className="content-container">
-          <h2>{t('GameDetails.description')}</h2>
-          <p>{parse(getTranslatedDescription(game.descriptions, game.description))}</p>
-          {game.necessities && game.necessities.length > 0 && (
-            <>
-              <h2>{t('GameDetails.necessities')}</h2>
-              <ul className="necessities-list">
-                {game.necessities.map((necessity) => {
-                  const translatedName = getTranslatedNecessityName(necessity);
-                  return translatedName ? <li key={necessity.necessity_id}>{translatedName}</li> : null
-                })}
-              </ul>
-            </>
+    <><Helmet>
+      <title>{getTranslatedName(game.translations, game.name)}</title>
+      <meta name="description" content={getTranslatedDescription(game.descriptions, game.description)} />
+      <meta property="og:title" content={getTranslatedName(game.translations, game.name)} />
+      <meta property="og:description" content={parse(getTranslatedDescription(game.descriptions, game.description))} />
+      <meta property="og:image" content={`${config.API_BASE_URL}/games/${game.image}`} />
+      <meta property="og:url" content={window.location.href} />
+      <link rel="canonical" href={window.location.href} />
+      <html lang={i18n.language} />
+    </Helmet><div className="main">
+        <div className="game-more-details">
+          <h1>{getTranslatedName(game.translations, game.name)}</h1>
+          <div className="img-wrapper">
+            <img
+              src={`${config.API_BASE_URL}/games/${game.image}`}
+              alt={game.name} />
+          </div>
+          {game.alias && (
+            <p className="alias">
+              {t('GameDetails.alias')}: {getTranslatedAlias(game.aliases, game.alias)}
+            </p>
           )}
-          <h2>{t('GameDetails.details')}</h2>
-          <p>
-            {t('GameDetails.minPlayers')}: {game.player_count}
-          </p>
-          <p>
-            {t('GameDetails.category')}: {getTranslatedName(game.categoryTranslations, game.category)}
-          </p>
-          <p>
-            {t('GameDetails.creator')}: {game.creator}
-          </p>
+          <div className="content-container">
+            <h2>{t('GameDetails.description')}</h2>
+            <p>{parse(getTranslatedDescription(game.descriptions, game.description))}</p>
+            {game.necessities && game.necessities.length > 0 && (
+              <>
+                <h2>{t('GameDetails.necessities')}</h2>
+                <ul className="necessities-list">
+                  {game.necessities.map((necessity) => {
+                    const translatedName = getTranslatedNecessityName(necessity);
+                    return translatedName ? <li key={necessity.necessity_id}>{translatedName}</li> : null;
+                  })}
+                </ul>
+              </>
+            )}
+            <h2>{t('GameDetails.details')}</h2>
+            <p>
+              {t('GameDetails.minPlayers')}: {game.player_count}
+            </p>
+            <p>
+              {t('GameDetails.category')}: {getTranslatedName(game.categoryTranslations, game.category)}
+            </p>
+            <p>
+              {t('GameDetails.creator')}: {game.creator}
+            </p>
+          </div>
         </div>
-      </div>
-    </div>
+      </div></>
   );
 }
 
--- src/components/GameList.js
+++ src/components/GameList.js
@@ -7,6 +7,7 @@ import 'rc-slider/assets/index.css';
 import Slider from 'rc-slider';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faSlidersH } from '@fortawesome/free-solid-svg-icons';
+import { Helmet } from "react-helmet";
 
 const GameList = () => {
   const [games, setGames] = useState([]);
@@ -125,94 +126,93 @@ const GameList = () => {
   }, [i18n]);
 
   return (
-    <div className="main">
-      <div className="game-list">
-        <h1>{t('GameList.title')}</h1>
-        <div className="search-bar-wrapper">
-          <input
-            type="text"
-            className="search-bar"
-            placeholder={t('GameList.searchPlaceholder')}
-            value={search}
-            onChange={handleSearchInputChange}
-          />
-          <FontAwesomeIcon className="search-icon" icon={faSearch} />
-          <button
-            className="filter-toggle"
-            onClick={() => setShowFilters(!showFilters)}
-            title={t('GameList.toggleFilters')}
-          >
-            <FontAwesomeIcon icon={faSlidersH} />
-          </button>
-        </div>
-      {showFilters && (
-        <div className="filters">
-          <div className="category-filter">
-            <h3>{t('GameList.categories')}</h3>
-            <div className="category-flexbox">
-              {categories.map((category) => (
-                <label key={category.id}>
-                  <input
-                    type="checkbox"
-                    checked={selectedCategories.has(category.id)}
-                    onChange={(e) => handleCategoryChange(category.id, e.target.checked)}
-                  />
-                  {getTranslatedName(category.translations, category.name)}
-                </label>
-              ))}
-            </div>
+    <><Helmet>
+      <title>DrinkMate</title>
+      <meta name="description" content="Discover and browse a wide variety of drinking games. Find the perfect game for your next party at DrinkMate." />
+    </Helmet><div className="main">
+        <div className="game-list">
+          <h1>{t('GameList.title')}</h1>
+          <div className="search-bar-wrapper">
+            <input
+              type="text"
+              className="search-bar"
+              placeholder={t('GameList.searchPlaceholder')}
+              value={search}
+              onChange={handleSearchInputChange} />
+            <FontAwesomeIcon className="search-icon" icon={faSearch} />
+            <button
+              className="filter-toggle"
+              onClick={() => setShowFilters(!showFilters)}
+              title={t('GameList.toggleFilters')}
+            >
+              <FontAwesomeIcon icon={faSlidersH} />
+            </button>
           </div>
-          <div className="player-count-filter">
-            <h3>{t('GameList.minCountPlayers')}</h3>
-            <div className="player-count-wrapper">
-              <div className="min-player-count" style={{ left: `${minLabelPosition}%` }}>
-                {minPlayerCount || minAvailablePlayerCount}
+          {showFilters && (
+            <div className="filters">
+              <div className="category-filter">
+                <h3>{t('GameList.categories')}</h3>
+                <div className="category-flexbox">
+                  {categories.map((category) => (
+                    <label key={category.id}>
+                      <input
+                        type="checkbox"
+                        checked={selectedCategories.has(category.id)}
+                        onChange={(e) => handleCategoryChange(category.id, e.target.checked)} />
+                      {getTranslatedName(category.translations, category.name)}
+                    </label>
+                  ))}
+                </div>
               </div>
-              <div className="max-player-count" style={{ left: `${maxLabelPosition}%` }}>
-                {maxPlayerCount || maxAvailablePlayerCount}
-              </div>
-              <div className="player-count-slider">
-                <Slider
-                  range
-                  min={minAvailablePlayerCount}
-                  max={maxAvailablePlayerCount}
-                  defaultValue={[minAvailablePlayerCount, maxAvailablePlayerCount]}
-                  onChange={(value) => {
-                    setMinPlayerCount(value[0]);
-                    setMaxPlayerCount(value[1]);
-                  }}
-                  allowCross={false}
-                />
+              <div className="player-count-filter">
+                <h3>{t('GameList.minCountPlayers')}</h3>
+                <div className="player-count-wrapper">
+                  <div className="min-player-count" style={{ left: `${minLabelPosition}%` }}>
+                    {minPlayerCount || minAvailablePlayerCount}
+                  </div>
+                  <div className="max-player-count" style={{ left: `${maxLabelPosition}%` }}>
+                    {maxPlayerCount || maxAvailablePlayerCount}
+                  </div>
+                  <div className="player-count-slider">
+                    <Slider
+                      range
+                      min={minAvailablePlayerCount}
+                      max={maxAvailablePlayerCount}
+                      defaultValue={[minAvailablePlayerCount, maxAvailablePlayerCount]}
+                      onChange={(value) => {
+                        setMinPlayerCount(value[0]);
+                        setMaxPlayerCount(value[1]);
+                      } }
+                      allowCross={false} />
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
+          )}
+          <ul>
+            {filteredGames().map((game) => (
+              <li key={game.id}>
+                <Link to={`/games/${game.id}`}>
+                  <div className="img-wrapper">
+                    <img
+                      src={`${config.API_BASE_URL}/games/${game.image}`}
+                      alt={game.name} />
+                  </div>
+                  <div className="game-info">
+                    <h2 className="game-title">
+                      {getTranslatedName(game.translations, game.name)}
+                      {game.new && <span className="new-indicator">New!</span>}
+                    </h2>
+                    <p className="game-details">
+                      {getTranslatedName(game.categoryTranslations, game.category)} • {game.player_count} {t('GameList.minPlayers')}
+                    </p>
+                  </div>
+                </Link>
+              </li>
+            ))}
+          </ul>
         </div>
-      )}
-        <ul>
-          {filteredGames().map((game) => (
-            <li key={game.id}>
-              <Link to={`/games/${game.id}`}>
-                <div className="img-wrapper">
-                  <img
-                    src={`${config.API_BASE_URL}/games/${game.image}`}
-                    alt={game.name}
-                  />
-                </div>
-                <div className="game-info">
-                  <h2 className="game-title">
-                    {getTranslatedName(game.translations, game.name)}
-                    {game.new && <span className="new-indicator">New!</span>}
-                  </h2>
-                  <p className="game-details">
-                    {getTranslatedName(game.categoryTranslations, game.category)} • {game.player_count} {t('GameList.minPlayers')}
-                  </p>
-                </div>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+      </div></>
   );
 };
 
