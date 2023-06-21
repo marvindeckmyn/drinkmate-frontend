e16a5938a58570b03cc75d648471053f8530bfcf
Marvin Deckmyn <marvindeckmyn@gmail.com>
2023-06-21 14:06:27 +0200

    Fix api base url

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000';
 
-export default API_BASE_URL;
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  API_BASE_URL
+};
