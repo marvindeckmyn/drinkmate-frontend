d8ea01b939b7ebd9f164386fcd0dda94b1a9a936
Marvin Deckmyn <marvindeckmyn@gmail.com>
2023-06-21 19:46:13 +0200

    Fix user sorting manage user admin privileges

--- src/components/admin/AdminGeneral.js
+++ src/components/admin/AdminGeneral.js
@@ -5,6 +5,7 @@ import { useTranslation } from 'react-i18next';
 
 const AdminGeneral = ({ authToken, currentUserId }) => {
   const [users, setUsers] = useState([]);
+  const [search, setSearch] = useState("");
   const { t } = useTranslation();
 
   const fetchUsers = useCallback(async () => {
@@ -14,12 +15,14 @@ const AdminGeneral = ({ authToken, currentUserId }) => {
           'x-auth-token': authToken,
         },
       });
-      setUsers(response.data.sort((a, b) => {
+      const sortedUsers = response.data.sort((a, b) => {
+        if (a.is_admin && !b.is_admin) return -1;
+        if (!a.is_admin && b.is_admin) return 1;
         if (a.id === currentUserId) return -1;
         if (b.id === currentUserId) return 1;
-        return 0;
-        })
-      );
+        return b.id - a.id;
+      });
+      setUsers(sortedUsers);
     } catch (err) {
       console.error(err);
     }
@@ -46,36 +49,65 @@ const AdminGeneral = ({ authToken, currentUserId }) => {
     }
   };
 
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredUsers = users.filter(user => 
+    user.username.toLowerCase().includes(search.toLowerCase())
+  );
+
+  const adminUsers = filteredUsers.filter(user => user.is_admin);
+  const recentUsers = search ? filteredUsers : filteredUsers.filter(user => !user.is_admin).slice(0, 10);
+
   return (
-      <div className="admin-general">
-        <h1>{t('AdminGeneral.title')}</h1>
-        <div className="manage-user-admin-privileges">
-          <h2>{t('AdminGeneral.manageUserAdminPrivileges')}</h2>
-          <ul>
-            {users.map((user) => {
-              return (
-                <li key={user.id}>
-                  <div className="user-info">
-                    <span className="username-email">
-                      {user.username} ({user.email})
-                    </span>
-                    <span className="admin-status">
-                      {user.is_admin ? t('AdminGeneral.admin') : t('AdminGeneral.notAdmin')}
-                    </span>
-                    <button
-                      onClick={() => toggleAdminPrivilege(user)}
-                      disabled={user.id === currentUserId}
-                    >
-                      {user.is_admin ? t('AdminGeneral.revokeAdmin') : t('AdminGeneral.grantAdmin')}
-                    </button>
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+    <div className="admin-general">
+      <h1>{t('AdminGeneral.title')}</h1>
+      <div className="manage-user-admin-privileges">
+      <h2>{t('AdminGeneral.manageUserAdminPrivileges')}</h2>
+      <input
+        type="text"
+        placeholder="Search users..."
+        value={search}
+        onChange={handleSearchChange}
+      />
+        <h3>{t('AdminGeneral.manageAdmins')}</h3>
+        <ul>
+          {adminUsers.map((user) => (
+            <li key={user.id}>
+              {renderUser(user, toggleAdminPrivilege, currentUserId, t)}
+            </li>
+          ))}
+        </ul>
+        <br/>
+        <h3>{t('AdminGeneral.manageRecentUsers')}</h3>
+        <ul>
+          {recentUsers.map((user) => (
+            <li key={user.id}>
+              {renderUser(user, toggleAdminPrivilege, currentUserId, t)}
+            </li>
+          ))}
+        </ul>
       </div>
+    </div>
   );
 };
+
+const renderUser = (user, toggleAdminPrivilege, currentUserId, t) => (
+  <div className="user-info">
+    <span className="username-email">
+      {user.username} ({user.email})
+    </span>
+    <span className="admin-status">
+      {user.is_admin ? t('AdminGeneral.admin') : t('AdminGeneral.notAdmin')}
+    </span>
+    <button
+      onClick={() => toggleAdminPrivilege(user)}
+      disabled={user.id === currentUserId}
+    >
+      {user.is_admin ? t('AdminGeneral.revokeAdmin') : t('AdminGeneral.grantAdmin')}
+    </button>
+  </div>
+);
 
 export default AdminGeneral;
